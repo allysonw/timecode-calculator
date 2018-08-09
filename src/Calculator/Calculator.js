36ad36d6cc1e34cb2412b4bb0a48fcf019f147e6
Allyson Wesman <allysonwesman@gmail.com>
2018-08-09 15:28:15 -0700

    add first component and App folder

---
+++ src/Calculator/Calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div>
+        Welcome!
+      </div>
+    );
+  }
+}
+
+export default Calculator;
